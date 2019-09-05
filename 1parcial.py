97c97d5040d85d6a2027bb12062b570cfc365370
Alvarhito05 <aovargas05@gmail.com>
2019-09-05 10:51:18 -0500

    Changes

--- 1parcial.py
+++ 1parcial.py
@@ -2,18 +2,37 @@ from flask import Flask, request
 import time
 app = Flask(__name__, static_folder='.', static_url_path='')
 
+
+class Singleton:
+	__instance = None
+	@staticmethod 
+	def getInstance():
+		""" Static access method. """
+		if Singleton.__instance == None:
+			Singleton()
+		return Singleton.__instance
+	def __init__(self):
+		""" Virtually private constructor. """
+		if Singleton.__instance != None:
+			raise Exception("This class is a singleton!")
+		else:
+			Singleton.__instance = self
+	def saveData(ti,te,hu):
+		time_string = time.strftime("%m%d%Y", time.localtime())
+
+		file_=open(time_string+".csv","a")
+
+		file_.write(str(time.strftime("%H:%M:%S", time.localtime()))+","+str(ti)+","+str(te)+","+str(hu)+"\n")
+		file_.close()
+
 @app.route("/log", methods=["GET", "POST"])
 def log():
 	ti=request.args.get("time")
 	te=request.args.get("temp")
 	hu=request.args.get("hum")
 	
-	time_string = time.strftime("%m%d%Y", time.localtime())
-
-	file_=open(time_string+".csv","a")
-
-	file_.write(str(time.strftime("%H:%M:%S", time.localtime()))+","+str(ti)+","+str(te)+","+str(hu)+"\n")
-	file_.close()
+	save=Singleton()
+	save.saveData(ti,te,hu)
 
 	return "Success"
 
